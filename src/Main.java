4671bce7926ff2b85066c4003040dd94db105878
Chris S <gummybassist@gmail.com>
2014-08-21 20:41:49 +0100

    Updated OBJC lexer to handle floats better, removed warnings

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -13,68 +16,64 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Main
 {
+	private static class ProcessRunnable implements Runnable
+	{
+		private String	mFilename;
 
+		public ProcessRunnable(String filename)
+		{
+			mFilename = filename;
+		}
+
+		@Override
+		public void run()
+		{
+			String resultKey = mFilename;
+			ComplexityResult complexityResult = createOrReuseResult(resultKey);
+
+			File sourceFile = new File(mFilename);
+			File headerFile = new File(mFilename.substring(0, mFilename.length() - 2) + ".h");
+
+			try
+			{
+				processFile(complexityResult, sourceFile);
+				if(headerFile.exists())
+				{
+					processFile(complexityResult, headerFile);
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			
+			processedFilesLatch.countDown();
+		}
+	}
+	
+	private static CountDownLatch processedFilesLatch;
 	private static HashMap<String, ComplexityResult>	complexityResults	= new HashMap<String, ComplexityResult>();
+	
+	private static Executor threadPool = Executors.newFixedThreadPool(4);
 
 	public static void main(String[] args)
 	{
+		processedFilesLatch = new CountDownLatch(args.length);
+		
 		for (int i = 0; i < args.length; i++)
 		{
-			// Remove the .h or .m from the end of the filename to form the key
-			// to the results
-			// hashmap
-			String resultKey = args[i].substring(0, args[i].length() - 2);
-
-			ComplexityResult complexityResult = createOrReuseResult(resultKey);
-
-			File sourceFile = new File(args[i]);
-			String sourceName = sourceFile.getName();
-			if (sourceName.endsWith(".m"))
-			{
-				List<Token> tokens = tokenise(args[i]);
-
-				for (Token token : tokens)
-				{
-					int type = token.getType();
-					if (type == ObjCLexer.IF || type == ObjCLexer.CASE || type == ObjCLexer.FOR || type == ObjCLexer.WHILE || type == ObjCLexer.QUESTION || type == ObjCLexer.AND || type == ObjCLexer.OR)
-					{
-						complexityResult.addBranch();
-					}
-					else if (type == ObjCLexer.IMPORT)
-					{
-						// Ensure we don't count the import of the sourcefile's
-						// associated header file
-						if (!token.getText().contains("\"" + sourceName.substring(0, sourceName.length() - 2) + ".h\""))
-						{
-							complexityResult.addDependency();
-						}
-					}
-				}
-			}
-			else if (sourceName.endsWith(".h"))
-			{
-				try
-				{
-					ParserRuleContext tree = generateAST(getTokenStreamForFile(args[i]));
-
-					if(foundSuperClass(tree))
-					{
-						complexityResult.addSuperClass();
-					}					
-				}
-				catch (IOException e)
-				{
-					System.err.println("Couldn't generate AST for: " + sourceName);
-					e.printStackTrace();
-				}
-
-			}
-			else
-			{
-				System.err.println("Invalid sourcefile provided: " + sourceName);
-			}
+			threadPool.execute(new ProcessRunnable(args[i]));
 		}
 
+		try
+		{
+			processedFilesLatch.await();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		
 		ComplexityResultJsonFormatter formatter = new ComplexityResultJsonFormatter();
 		for (Entry<String, ComplexityResult> results : complexityResults.entrySet())
 		{
@@ -82,18 +81,50 @@ public class Main
 		}
 	}
 
+	private static void processFile(ComplexityResult complexityResult, File sourceFile) throws IOException
+	{
+		CommonTokenStream tokenStream = getTokenStreamForFile(sourceFile.getAbsolutePath());
+		List<Token> tokens = tokenStream.getTokens();
+
+		for (Token token : tokens)
+		{
+			int type = token.getType();
+			if (type == ObjCLexer.IF || type == ObjCLexer.CASE || type == ObjCLexer.FOR || type == ObjCLexer.WHILE || type == ObjCLexer.QUESTION || type == ObjCLexer.AND || type == ObjCLexer.OR)
+			{
+				complexityResult.addBranch();
+			}
+			else if (type == ObjCLexer.IMPORT)
+			{
+				// Ensure we don't count the import of the sourcefile's
+				// associated header file
+				if (!token.getText().contains("\"" + sourceFile.getName().substring(0, sourceFile.getName().length() - 2) + ".h\""))
+				{
+					complexityResult.addDependency();
+				}
+			}
+		}
+
+		ParserRuleContext tree = generateAST(tokenStream);
+
+		if (foundSuperClass(tree))
+		{
+			complexityResult.addSuperClass();
+		}
+
+	}
+
 	private static boolean foundSuperClass(ParseTree branch)
 	{
-		if(branch.getPayload() instanceof ObjCParser.Superclass_nameContext)
+		if (branch.getPayload() instanceof ObjCParser.Superclass_nameContext)
 		{
 			return !branch.getText().equals("NSObject");
 		}
-		
+
 		boolean result = false;
-		
+
 		for (int i = 0; i < branch.getChildCount(); i++)
 		{
-			if(foundSuperClass(branch.getChild(i)))
+			if (foundSuperClass(branch.getChild(i)))
 			{
 				result = true;
 				break;
@@ -117,21 +148,6 @@ public class Main
 	{
 		ObjCParser parser = new ObjCParser(tokenStream);
 		return parser.lex();
-	}
-
-	private static List<Token> tokenise(String filename)
-	{
-		List<Token> result = null;
-		try
-		{
-			result = getTokenStreamForFile(filename).getTokens();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-
-		return result;
 	}
 
 	private static CommonTokenStream getTokenStreamForFile(String filename) throws IOException
